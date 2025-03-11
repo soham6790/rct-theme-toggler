4de071a2fb484767584bfa0ec8e8f78b5225d34a
soham <sohamdbu@bu.edu>
2025-03-11 00:01:52 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>Test</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
