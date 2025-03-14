48df37b2232ca38814c90b59ace164c079a715c3
soham <sohamdbu@bu.edu>
2025-03-13 22:06:12 -0400

    Modifyinf app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
 import Login from './components/Login'
 import Profile from './components/Profile'
+import { ThemeProvider } from './context/Theme'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/card'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [themeMode,setThemeMode] = useState("light"); 
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+  useEffect(() => {
+
+    const htmlClassList = document.querySelector('html').classList;
+    htmlClassList.remove("light","dark");
+    htmlClassList.add(themeMode); 
+
+  },[themeMode]);
 
   return (
-    <UserContextProvider>
-      <h1>Test Application</h1>
-      <Login />
-      <Profile />
-    </UserContextProvider>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
